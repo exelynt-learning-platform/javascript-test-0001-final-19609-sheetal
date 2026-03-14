91a8c47b9478a21ee217ce114f395dac1924e214
SheetalGawade26 <sheetalgawade2611@gmail.com>
2026-03-14 12:38:06 +0530

    Pattern Code

---
+++ pattern.js
@@ -0,0 +1,42 @@
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+    let row = "";
+
+    
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+    row += "*";
+
+    if (i > 1) {
+        for (let j = 1; j <= 2 * i - 3; j++) {
+            row += " ";
+        }
+        
+        row += "*";
+    }
+
+    console.log(row);
+}
+
+for (let i = n - 1; i >= 1; i--) {
+    let row = "";
+
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+    row += "*";
+
+    if (i > 1) {
+        for (let j = 1; j <= 2 * i - 3; j++) {
+            row += " ";
+        }
+       
+        row += "*";
+    }
+
+    console.log(row);
+}
